2de013576928968e32594744e6c4c6b271c57967
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-24 22:54:46 +0530

    Update py13.py

--- py13.py
+++ py13.py
@@ -2,33 +2,33 @@
 #tuple = a collection which is ordered and unchangable!
 #           used to group together related data!
 
-#gamer = ("mortal",30,"male")
+gamer = ("mortal",30,"male")
 #some tuple functions
-#print(gamer.count("mortal"))
-#print(gamer.index("mortal"))
+print(gamer.count("mortal"))
+print(gamer.index("mortal"))
 # we can use a for loop to explore the contents in tuple!
 
-#for x in gamer:
- #   print(x)
-#for x in gamer:
- #   print(x,end=" ")
-#if 20 in gamer:
-#    print("mortal is here!")
-#else:
-#   print("mortal not found:("))
+for x in gamer:
+    print(x)
+for x in gamer:
+    print(x,end=" ")
+if 20 in gamer:
+    print("mortal is here!")
+else:
+   print("mortal not found:("))
 
 
 # tuple = a collection which is ordered and unchangalbe!
 #          used to group together related data!
 
-#student = ("dude",21,"male")
+student = ("dude",21,"male")
 # some tuple functions!
-#print(student.count("male"))
-#print(student.index("male"))
+print(student.count("male"))
+print(student.index("male"))
 
-#for x in student:
-#    print(x)
-#if "dud" in student:
-#    print("dude is here!")
-#else:
-#    print("not here!")
+for x in student:
+    print(x)
+if "dud" in student:
+    print("dude is here!")
+else:
+    print("not here!")
